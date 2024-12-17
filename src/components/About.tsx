baed80d561057295df5c4bd4f54f923e0c9895d8
Ulises Molina <uli.molina10@gmail.com>
2024-12-17 20:16:43 -0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,7 +16,7 @@ export const About:React.FC<ComponentProps> = ({lenguage,menu}) => {
     className="flex flex-col xl:h-[120vh] 2xl:min-h-[80vh] md:mt-72 lg:mt-16 gap-24 lg:gap-32 justify-center items-center">
         <h3 className="text-3xl lg:text-4xl font-medium">{lenguage === "EN" ? "About me" : "Sobre mi"}</h3>
         <div className="flex flex-col lg:w-1/3 border-gray-500 border-2 p-8 rounded-lg gap-20 shadow-lg">
-            {lenguage === "EN" ? <p className="text:lg lg:text-md">Busco adentrarme en el mundo IT como desarrollador frontend, soy una persona motivada a crecer, aprender y
+            {lenguage === "ES" ? <p className="text:lg lg:text-md">Busco adentrarme en el mundo IT como desarrollador frontend, soy una persona motivada a crecer, aprender y
 desarrollarme en distintas áreas. <br></br><br></br>Me considero
 responsable, dinámico, creativo y confiable con
 facilidad de adaptación y capacidad de trabajar en
