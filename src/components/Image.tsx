dbda90f1e7a223658cce2fd968fdbbe2db4c55c4
Ulises Molina <uli.molina10@gmail.com>
2024-12-27 01:38:32 -0300

    fix bug images

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -4,8 +4,8 @@ import { motion } from "motion/react"
 const Image:React.FC = () => {
     return (
         <motion.img
-        initial ={{ opacity: 0,translateX: -50, rotate: -5 }}
-        animate ={{ opacity: 1,translateX: 0, rotate: 0 }}
+        initial ={{ opacity: 0, }}
+        animate ={{ opacity: 1, }}
         transition={{ duration: 1.5 }}
         className="filter brightness-115 contrast-115 saturate-110 w-2/5 rounded-2xl min-h-56 min-w-56 lg:min-h-0 lg:min-w-0 center object-contain" src="/foto.webp" alt="foto de Ulises Molina"></motion.img>
     )
