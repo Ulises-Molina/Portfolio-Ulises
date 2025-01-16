052d52a1b2e082a90800e36f8d026856aa2aaaac
Ulises Molina <uli.molina10@gmail.com>
2025-01-16 17:39:03 -0300

    change in cv

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -12,7 +12,7 @@ export const Main: React.FC<ComponentProps> = ({lenguaje,menu}) => {
     return (
         <>
         {menu ? null : <main id="home" className=" flex flex-col lg:flex-row sm:min-h-[90vh] lg:overflow-hidden lg:justify-normal gap-10 lg:gap-0 justify-center items-center mt-14 lg:mt-0">
-            <div className="hidden w-1/2 md:flex justify-center items-center">
+            <div className="items-center justify-center hidden w-1/2 md:flex">
             <Suspense fallback={<ProgressSpinner/>}>    
                 <LazyImage/>
             </Suspense>
@@ -23,9 +23,9 @@ export const Main: React.FC<ComponentProps> = ({lenguaje,menu}) => {
             initial ={{ opacity: 0,translateY: 50 }}
             animate ={{ opacity: 1,translateY: 0 }}
             transition={{ duration: 2.5 }}
-            className="lg:w-1/3 flex flex-col justify-center gap-3 lg:gap-7 self-start mx-8 lg:self-center">
-                <h1 className="text-2xl 2xl:text-4xl font-bold">{lenguaje === "EN" ? "Hi, I'm Ulises Molina" : "Hola, soy Ulises Molina"}</h1>
-                <h2 className="2xl:text-xl text-lg font-bold">{lenguaje === "EN" ? "I'm a Frontend developer from Buenos Aires, Argentina" : "Desarrollador frontend de Buenos Aires, Argentina"}</h2>
+            className="flex flex-col self-start justify-center gap-3 mx-8 lg:w-1/3 lg:gap-7 lg:self-center">
+                <h1 className="text-2xl font-bold 2xl:text-4xl">{lenguaje === "EN" ? "Hi, I'm Ulises Molina" : "Hola, soy Ulises Molina"}</h1>
+                <h2 className="text-lg font-bold 2xl:text-xl">{lenguaje === "EN" ? "I'm a Frontend developer from Buenos Aires, Argentina" : "Desarrollador frontend de Buenos Aires, Argentina"}</h2>
                 <p className="2xl:text-lg text-md">{lenguaje === "EN" ? "I like to be in constant growth with the ability to learn new technologies and adapt easily to new work teams. Passionate about challenges and even more so  if they enrich my knowledge. Completely self-taught." : "Me gusta estar en constante crecimiento con capacidad de aprender nuevas tecnologías y adaptarme fácilmente a nuevos equipos de trabajo. Apasionado por los retos y más si enriquecen mis conocimientos. Completamente autodidacta."}</p>
                 <div className="flex flex-col gap-10 mt-10 ">
                     <p className="2xl:text-xl text-md">{lenguaje === "EN" ? "Principal technologies" : "Tecnologías principales"}</p>
