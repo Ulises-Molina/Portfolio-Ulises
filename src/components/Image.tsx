e4c1ac70d862f941c21de892a593a623d5b02cbd
Ulises Molina <uli.molina10@gmail.com>
2025-01-22 01:07:43 -0300

    fix vercel deploy bug

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -4,11 +4,18 @@ import { motion } from "motion/react"
 const Image:React.FC = () => {
     return (
         <motion.img
-        initial ={{ opacity: 0, }}
-        animate ={{ opacity: 1, }}
-        transition={{ duration: 1.5 }}
-        className="filter brightness-115 contrast-115 saturate-110 w-2/5 rounded-2xl min-h-56 min-w-56 lg:min-h-0 lg:min-w-0 center object-contain drop-shadow-lg" src="/foto.webp" alt="foto de Ulises Molina"></motion.img>
-    )
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5 }}
+          className="object-contain w-2/5 filter rounded-2xl min-h-56 min-w-56 lg:min-h-0 lg:min-w-0 center drop-shadow-lg"
+          style={{
+            filter: 'brightness(0.8) contrast(1.1) saturate(1.1) sepia(0.2)',
+          }}
+          src="/foto.webp"
+          alt="foto de Ulises Molina"
+        />
+      );
+      
 }
 
 export default Image;
